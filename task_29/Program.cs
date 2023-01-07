6c0e4bb56560f5c2bba0b932f1eb615183142c5e
Nikolai <luzinnikolai@mail.ru>
2023-01-07 20:27:42 +0500

    задача 29

---
+++ task_29/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран.
+
+void PrintArray()
+{
+    int length = ReadInt("length");
+    int[] array = GetArray(length);
+    Console.Write(ArrayToString(array));
+}
+
+
+PrintArray();
+
+
+int ReadInt(string argumentName)
+{
+	Console.Write($"Input {argumentName}: ");
+	return int.Parse(Console.ReadLine()!);
+}
+
+
+int[] GetArray(int length)
+{
+	int[] array = new int[length];
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = ReadInt($"element {i}");
+	}
+
+	return array;
+}
+
+
+string ArrayToString(int[] array)
+{
+	string result = string.Empty;
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		result += $"{array[i]}, ";
+	}
+	return result;
+}
